ce3f5204cfc099b9674a48484d7f52cb8da0186a
Rachid BOUIKILA <rachid.bouikila@accor.com>
2018-10-04 14:40:55 +0200

    add ec2 config

--- layer-cloudera/main.tf
+++ layer-cloudera/main.tf
@@ -10,3 +10,29 @@ data "terraform_remote_state" "layer-base" {
     region = "eu-west-1"
   }
 }
+
+data "aws_ami" "redhat" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["redhat/images/hvm-ssd/RHEL-7.5_HVM_GA*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["309956199498"] # Red hat
+}
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+  count = 3
+
+  tags {
+    Name = "HelloWorld"
+  }
+}
