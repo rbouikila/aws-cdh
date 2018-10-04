350666815e644261a8982be132895a0dd99f9c96
Rachid BOUIKILA <rachid.bouikila@accor.com>
2018-10-04 14:16:52 +0200

    Push initial code

---
+++ layer-base/output.tf
@@ -0,0 +1,23 @@
+output "sn_public_a_id" {
+  value = "${aws_subnet.demo_sn_public_a.id}"
+}
+
+output "sn_public_b_id" {
+  value = "${aws_subnet.demo_sn_public_b.id}"
+}
+
+output "sn_public_c_id" {
+  value = "${aws_subnet.demo_sn_public_c.id}"
+}
+
+output "vpc_id" {
+  value = "${aws_vpc.demo_vpc.id}"
+}
+
+output "vpc_cidr" {
+  value = "${var.vpc_cidr}"
+}
+
+output "private_dns_zone" {
+  value = "${var.private_dns_zone}"
+}
