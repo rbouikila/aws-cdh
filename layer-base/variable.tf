a438d033198dfcc497fdd0f0705921b7c7ff06b5
Rachid BOUIKILA <rachid.bouikila@accor.com>
2018-10-04 20:41:52 +0200

    fixe

--- layer-base/variable.tf
+++ layer-base/variable.tf
@@ -1,21 +1,21 @@
 
-variable "account_id" {
-  default = "549637939820"
-}
-
+# AWS Region
 variable "region" {
   default = "eu-west-1"
 }
 
+# Private DNS zone
 variable "private_dns_zone" {
   default = "cloudera.accorhotels"
 }
 
+# VPC CIDR
 variable "vpc_cidr" {
   description = "CIDR for the whole VPC"
   default     = "10.0.0.0/16"
 }
 
+# Public subnet
 variable "public_subnet_a_cidr" {
   description = "CIDR for the Public Subnet in a AZ"
   default     = "10.0.1.0/24"
@@ -31,6 +31,7 @@ variable "public_subnet_c_cidr" {
   default     = "10.0.3.0/24"
 }
 
+# Private subnet
 variable "private_subnet_a_cidr" {
   description = "CIDR for the Private Subnet in a AZ"
   default     = "10.0.4.0/24"
