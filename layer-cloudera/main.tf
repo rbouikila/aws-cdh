a477f26b876dd517632f797e8df9781512a01c48
Rachid BOUIKILA <rachid.bouikila@accor.com>
2018-10-04 14:50:54 +0200

    fixe

--- layer-cloudera/main.tf
+++ layer-cloudera/main.tf
@@ -8,7 +8,7 @@ variable "region" {
 
 data "terraform_remote_state" "layer-base" {
  backend = "s3"
- bucket   config {
+ config {
     bucket = "cloudera-deploiement-terraform"
     key    = "layer-base"
     region = "eu-west-1"
